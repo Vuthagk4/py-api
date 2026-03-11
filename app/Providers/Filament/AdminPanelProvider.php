742011fcc4acb34e3d48ede85d03a20c6d1846c5
Thany168 <ounthany5@gmail.com>
2026-03-11 10:54:45 +0700

    Update Filament admin panel branding

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('PhumYerng Shop')
             ->colors([
                 'primary' => Color::Amber,
             ])
